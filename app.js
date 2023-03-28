7fe3a51cdce93b93be4386ad2936fe8e5de6f672
enterw <igor.wenter@gmail.com>
2023-03-28 10:34:03 +0200

    Update app.js
    settimo tentativo no www to www

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ app.set('view engine', 'jade');
 
 i18n.registerAppHelper(app);
 
+app.use((req, res, next) => {
+  console.log('Time:', Date.now());
+  
+  if (req.headers.host.slice(0, 4) === 'www.') {
+      var newHost = req.headers.host.slice(4);
+      return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+  }
+  
+  next();
+});
+
 app.get('/', function (req, res) {
     res.redirect(301, '/it/');
 });
@@ -72,17 +83,6 @@ app.get('/cookie', function(req,res){
 app.get('*', function(req, res){
   res.redirect(301, '/it/');
   //res.send('404:Pagina non disponibile!', 404);
-});
-
-app.use((req, res, next) => {
-  console.log('Time:', Date.now());
-  
-  if (req.headers.host.slice(0, 4) === 'www.') {
-      var newHost = req.headers.host.slice(4);
-      return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-  }
-  
-  next();
 });
 
 function logErrors(err, req, res, next) {
